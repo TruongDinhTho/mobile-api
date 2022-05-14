66dd95a28279eea9d7908675992323954c238c6a
TruongDinhTho <43953312+TruongDinhTho@users.noreply.github.com>
2022-05-14 07:55:24 +0700

    update server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const jsonServer = requie('json-server');
+const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -7,4 +7,7 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+// server.listen(port);
+server.listen(port, function(){
+    console.log(`Application is listening on port ${port}!`)
+  });
